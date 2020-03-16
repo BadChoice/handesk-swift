6b6f9bd1cf0aac69632e5a2ae6e9b9f5230a0c35
Jordi Puigdellívol <jordi@gloobus.net>
2020-03-16 08:45:44 +0100

    Initial Commit

---
+++ Handesk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Handesk
+//
+//  Created by Jordi Puigdellívol on 16/03/2020.
+//  Copyright © 2020 Revo Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
